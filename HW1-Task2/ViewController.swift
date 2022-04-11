41acab0d276fc2d0788fcc861023ca7edf8de835
Ramir <amrayev.ramir@icloud.com>
2022-04-12 01:08:31 +0600

    Initial Commit

---
+++ HW1-Task2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW1-Task2
+//
+//  Created by Ramir Amrayev on 4/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
