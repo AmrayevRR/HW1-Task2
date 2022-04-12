85788cf422897151cc97e281949ae955ea9a27b6
Ramir <amrayev.ramir@icloud.com>
2022-04-12 16:12:48 +0600

    Second task almost done

---
+++ HW1-Task2/ChangePasswordViewController.swift
@@ -0,0 +1,262 @@
+//
+//  ChangePasswordViewController.swift
+//  HW1-Task2
+//
+//  Created by Ramir Amrayev on 4/12/22.
+//
+
+import UIKit
+import SnapKit
+
+class ChangePasswordViewController: UIViewController {
+    
+    let backgroundColor = UIColor(red: 247, green: 247, blue: 247, alpha: 1)
+    
+    let currentPasswordTextField = CustomTextField()
+    var currentPasswordIconIsClick = false
+    let currentPasswordImageView = UIImageView()
+    let newPasswordTextField = CustomTextField()
+    var newPasswordIconIsClick = false
+    let newPasswordImageView = UIImageView()
+    let repeatPasswordTextField = CustomTextField()
+    let forgotLabel = UnderlinedLabel()
+    let saveButton = UIButton()
+    
+    var bottomButtonConstraint = NSLayoutConstraint()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+
+        layoutUI()
+        // Do any additional setup after loading the view.
+    }
+    
+    @objc private func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            self.bottomButtonConstraint.constant -= keyboardSize.height
+        }
+    }
+
+    @objc private func keyboardWillHide(notification: NSNotification) {
+        self.bottomButtonConstraint.constant = 0
+    }
+    
+    
+    private func layoutUI() {
+        view.backgroundColor = backgroundColor
+        title = "Изменить пароль"
+        
+        view.addSubview(currentPasswordTextField)
+        view.addSubview(newPasswordTextField)
+        view.addSubview(repeatPasswordTextField)
+        view.addSubview(forgotLabel)
+        view.addSubview(saveButton)
+        
+        layoutCurrentPasswordTextField()
+        layoutNewPasswordTextField()
+        layoutRepeatPasswordTextField()
+        layoutForgotLabel()
+        layoutSaveButton()
+    }
+    
+    private func layoutCurrentPasswordTextField() {
+        
+        currentPasswordTextField.isSecureTextEntry = true
+        currentPasswordTextField.placeholder = "Текущий пароль"
+        
+        currentPasswordTextField.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.width.equalToSuperview().multipliedBy(0.9)
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(30)
+            $0.height.equalTo(50)
+        }
+        
+        currentPasswordTextField.backgroundColor = .white
+        
+        setRighImageToTextField(textField: currentPasswordTextField, imageView: currentPasswordImageView, action: #selector(currentPasswordImageViewTapped))
+    }
+    
+    private func setRighImageToTextField(textField: UITextField, imageView: UIImageView, action: Selector) {
+        let image = UIImage(systemName: "eye.slash")
+        imageView.image = image
+        imageView.tintColor = .darkGray
+        let contentView = UIView()
+        contentView.addSubview(imageView)
+        contentView.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
+        imageView.frame = CGRect(x: -10, y: 0, width: 30, height: 20)
+        
+        textField.rightView = contentView
+        textField.rightViewMode = .always
+        
+        let tap = UITapGestureRecognizer(target: self, action: action)
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(tap)
+    }
+    
+    private func layoutNewPasswordTextField() {
+        
+        newPasswordTextField.isSecureTextEntry = true
+        newPasswordTextField.placeholder = "Новый пароль"
+        
+        newPasswordTextField.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.size.equalTo(currentPasswordTextField)
+            $0.top.equalTo(currentPasswordTextField.snp.bottom).offset(15)
+        }
+        
+        newPasswordTextField.backgroundColor = .white
+        
+        setRighImageToTextField(textField: newPasswordTextField, imageView: newPasswordImageView, action: #selector(newPasswordImageViewTapped))
+    }
+    
+    private func layoutRepeatPasswordTextField() {
+        repeatPasswordTextField.isSecureTextEntry = true
+        repeatPasswordTextField.placeholder = "Повторить новый пароль"
+        
+        repeatPasswordTextField.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.size.equalTo(currentPasswordTextField)
+            $0.top.equalTo(newPasswordTextField.snp.bottom).offset(15)
+        }
+        
+        repeatPasswordTextField.backgroundColor = .white
+    }
+    
+    private func layoutForgotLabel() {
+        forgotLabel.text = "Забыли пароль ?"
+        forgotLabel.textColor = .gray
+        
+        forgotLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(repeatPasswordTextField.snp.bottom).offset(20)
+        }
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(forgotLabelTapped))
+        forgotLabel.isUserInteractionEnabled = true
+        forgotLabel.addGestureRecognizer(tap)
+    }
+    
+    private func layoutSaveButton() {
+        saveButton.setTitle("Сохранить", for: .normal)
+        saveButton.setTitleColor(.gray, for: .normal)
+        saveButton.backgroundColor = .white
+        
+        saveButton.layer.cornerRadius = 10
+        saveButton.layer.borderWidth = 1
+        saveButton.layer.borderColor = UIColor.lightGray.cgColor
+        
+        saveButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.size.equalTo(currentPasswordTextField)
+//            $0.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        bottomButtonConstraint = saveButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        bottomButtonConstraint.isActive = true
+        
+        saveButton.addTarget(self, action: #selector(saveButtonAction), for: .touchUpInside)
+    }
+    
+    @objc func currentPasswordImageViewTapped(tap: UITapGestureRecognizer) {
+        if currentPasswordIconIsClick {
+            currentPasswordIconIsClick = false
+            currentPasswordImageView.image = UIImage(systemName: "eye.slash")
+            currentPasswordTextField.isSecureTextEntry = true
+        }
+        else {
+            currentPasswordIconIsClick = true
+            currentPasswordImageView.image = UIImage(systemName: "eye")
+            currentPasswordTextField.isSecureTextEntry = false
+        }
+    }
+    @objc func newPasswordImageViewTapped(tap: UITapGestureRecognizer) {
+        if newPasswordIconIsClick {
+            newPasswordIconIsClick = false
+            newPasswordImageView.image = UIImage(systemName: "eye.slash")
+            newPasswordTextField.isSecureTextEntry = true
+        }
+        else {
+            newPasswordIconIsClick = true
+            newPasswordImageView.image = UIImage(systemName: "eye")
+            newPasswordTextField.isSecureTextEntry = false
+        }
+    }
+    @objc func forgotLabelTapped(tap: UITapGestureRecognizer) {
+        print("Forgot label tapped")
+    }
+    @objc func saveButtonAction(sender: UIButton!) {
+      print("Save button tapped")
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+@IBDesignable
+class CustomTextField: UITextField {
+    
+    @IBInspectable var rightPadding: CGFloat = 5
+    
+//     Provides right padding for images
+    override func rightViewRect(forBounds bounds: CGRect) -> CGRect {
+        var textRect = super.rightViewRect(forBounds: bounds)
+        textRect.origin.x -= rightPadding
+        return textRect
+    }
+    
+    let insets: UIEdgeInsets
+    
+    init(insets: UIEdgeInsets = UIEdgeInsets(top: 0, left: 12, bottom: 0, right: 39)) {
+        self.insets = insets
+        super.init(frame: .zero)
+        
+        layer.cornerRadius = 8
+        layer.borderWidth = 1.5
+        layer.borderColor = UIColor.lightGray.cgColor
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: insets)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: insets)
+    }
+}
+
+class UnderlinedLabel: UILabel {
+
+override var text: String? {
+    didSet {
+        guard let text = text else { return }
+        let textRange = NSRange(location: 0, length: text.count)
+        let attributedText = NSMutableAttributedString(string: text)
+        attributedText.addAttribute(.underlineStyle,
+                                    value: NSUnderlineStyle.single.rawValue,
+                                    range: textRange)
+        // Add other attributes if needed
+        self.attributedText = attributedText
+        }
+    }
+}
+
+protocol KeyboardObserving: class {
+    func keyboardWillShow(_ notification: Notification)
+    func keyboardWillHide(_ notification: Notification)
+}
--- HW1-Task2/ViewController.swift
+++ HW1-Task2/ViewController.swift
@@ -6,14 +6,319 @@
 //
 
 import UIKit
+import SnapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    
+    let backgroundColor = UIColor(red: 247, green: 247, blue: 247, alpha: 1)
+    
+    let imageView = UIImageView()
+    let cameraView = UIImageView()
+    let fullNameLabel = UILabel()
+    let emailLabel = UILabel()
+    let firstSeparator = UIView()
+    let pushLabel = UILabel()
+    let pushSwitch = UISwitch()
+    let secondSeparator = UIView()
+    let passwordStack = UIStackView()
+    let changePasswordLabel = UILabel()
+    let thirdSeparator = UIView()
+    let codeStack = UIStackView()
+    let changeCodeLabel = UILabel()
+    let fourthSeparator = UIView()
+    let entryLabel = UILabel()
+    let entrySwitch = UISwitch()
+    let fifthSeparator = UIView()
+    let phoneStack = UIStackView()
+    let phoneLabel = UILabel()
+    let sixthSeparator = UIView()
+    let exitLabel = UILabel()
+    let seventhSeparator = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        layoutUI()
     }
 
+    private func layoutUI() {
+        title = "Профиль"
+        
+        view.backgroundColor = backgroundColor
+        navigationController?.navigationBar.barTintColor = backgroundColor
+        
+        view.addSubview(imageView)
+        view.addSubview(cameraView)
+        view.addSubview(fullNameLabel)
+        view.addSubview(emailLabel)
+        view.addSubview(firstSeparator)
+        view.addSubview(pushLabel)
+        view.addSubview(pushSwitch)
+        view.addSubview(secondSeparator)
+        view.addSubview(passwordStack)
+        view.addSubview(thirdSeparator)
+        view.addSubview(codeStack)
+        view.addSubview(fourthSeparator)
+        view.addSubview(entryLabel)
+        view.addSubview(entrySwitch)
+        view.addSubview(fifthSeparator)
+        view.addSubview(phoneStack)
+        view.addSubview(sixthSeparator)
+        view.addSubview(exitLabel)
+        view.addSubview(seventhSeparator)
+        
+        layoutImageView()
+        layoutCameraView()
+        layoutFullNameLabel()
+        layoutEmailLabel()
+        layoutFirstSeparator()
+        layoutLabelWithSwitch(label: pushLabel, text: "Push уведомления", separator: firstSeparator)
+        layoutSwitchWithLabel(mySwitch: pushSwitch, label: pushLabel, action: #selector(pushSwitchValueDidChange))
+        layoutSeparatorUnderLabel(separator: secondSeparator, label: pushLabel)
+        layoutStackWithLeftArrow(stack: passwordStack, label: changePasswordLabel, text: "Изменить пароль", action: #selector(handlePasswordStackTap), separatorTop: secondSeparator)
+        layoutSeparatorUnderStack(separator: thirdSeparator, stack: passwordStack)
+        layoutStackWithLeftArrow(stack: codeStack, label: changeCodeLabel, text: "Изменить код быстрого доступа", action: #selector(handleCodeStackTap), separatorTop: thirdSeparator)
+        layoutSeparatorUnderStack(separator: fourthSeparator, stack: codeStack)
+        layoutLabelWithSwitch(label: entryLabel, text: "Вход с Face/Touch ID", separator: fourthSeparator)
+        layoutSwitchWithLabel(mySwitch: entrySwitch, label: entryLabel, action: #selector(entrySwitchValueDidChange))
+        layoutSeparatorUnderLabel(separator: fifthSeparator, label: entryLabel)
+        layoutStackWithLeftArrow(stack: phoneStack, label: phoneLabel, text: "Изменить номер телефона", action: #selector(handlePhoneStackTap), separatorTop: fifthSeparator)
+        layoutSeparatorUnderStack(separator: sixthSeparator, stack: phoneStack)
+        layoutLabelWithSwitch(label: exitLabel, text: "Выход", separator: sixthSeparator)
+        layoutSeparatorUnderLabel(separator: seventhSeparator, label: exitLabel)
+        
+        configureExitLabel()
+    }
+    
+    private func layoutImageView() {
+        imageView.image = UIImage(systemName: "person.circle")
+        imageView.backgroundColor = .white
+        
+        imageView.layer.cornerRadius = view.frame.size.height * 0.05
+//        imageView.layer.borderColor = .init(red: 0, green: 0, blue: 0, alpha: 1)
+//        imageView.layer.borderWidth = 1
+        
+        imageView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaInsets).offset(100)
+            $0.height.equalToSuperview().multipliedBy(0.1)
+            $0.width.equalTo(imageView.snp.height)
+            $0.centerX.equalToSuperview()
+        }
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleImageViewTap))
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(tap)
+    }
+    
+    private func layoutCameraView() {
+        cameraView.image = UIImage(systemName: "camera.circle")
+        cameraView.backgroundColor = .blue
+        cameraView.tintColor = .white
+        
+        cameraView.layer.cornerRadius = view.frame.size.height * 0.02
+        
+        cameraView.snp.makeConstraints {
+            $0.trailing.equalTo(imageView)
+            $0.bottom.equalTo(imageView)
+            $0.height.equalToSuperview().multipliedBy(0.04)
+            $0.width.equalTo(cameraView.snp.height)
+        }
+    }
+    
+    private func layoutFullNameLabel() {
+        fullNameLabel.text = "Мария Атрисова"
+        fullNameLabel.font = .boldSystemFont(ofSize: 18)
+        fullNameLabel.textColor = .darkGray
+        
+        fullNameLabel.snp.makeConstraints {
+            $0.top.equalTo(imageView.snp.bottom).offset(10)
+            $0.centerX.equalToSuperview()
+        }
+    }
+    
+    private func layoutEmailLabel() {
+        emailLabel.text = "maria.atrisova@gmail.com"
+        emailLabel.textColor = .darkGray
+        
+        emailLabel.snp.makeConstraints {
+            $0.top.equalTo(fullNameLabel.snp.bottom).offset(4)
+            $0.centerX.equalToSuperview()
+        }
+    }
+    
+    private func layoutFirstSeparator() {
+        firstSeparator.backgroundColor = .gray
+        
+        firstSeparator.snp.makeConstraints {
+            $0.top.equalTo(emailLabel.snp.bottom).offset(20)
+            $0.height.equalTo(1)
+            $0.leading.equalToSuperview().offset(15)
+            $0.trailing.equalToSuperview().inset(15)
+        }
+    }
+    
+    private func layoutLabelWithSwitch(label: UILabel, text: String, separator: UIView) {
+        label.text = text
+        label.textColor = .gray
+        
+        label.snp.makeConstraints {
+            $0.top.equalTo(separator.snp.bottom).offset(20)
+            $0.leading.equalToSuperview().offset(15)
+        }
+    }
+    
+    private func layoutSwitchWithLabel(mySwitch: UISwitch, label: UILabel, action: Selector) {
+        mySwitch.setOn(true, animated: true)
+        mySwitch.addTarget(self, action: action, for: .valueChanged)
+        mySwitch.onTintColor = .systemPurple
+        
+        mySwitch.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(15)
+            $0.centerY.equalTo(label)
+        }
+    }
+    
+    private func layoutSeparatorUnderLabel(separator: UIView, label: UILabel) {
+        separator.backgroundColor = .gray
+        
+        separator.snp.makeConstraints {
+            $0.top.equalTo(label.snp.bottom).offset(20)
+            $0.height.equalTo(1)
+            $0.leading.equalToSuperview().offset(15)
+            $0.trailing.equalToSuperview().inset(15)
+        }
+    }
+    
+    private func layoutStackWithLeftArrow(stack: UIStackView, label: UILabel, text: String, action: Selector, separatorTop: UIView) {
+        stack.axis = .horizontal
+        
+        stack.addSubview(label)
+        
+        stack.snp.makeConstraints {
+            $0.top.equalTo(separatorTop.snp.bottom)
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+            $0.height.equalTo(60)
+        }
+        
+        layoutStackLabel(label: label, text: text)
+        layoutStackLeftArrow(stack: stack)
+        
+        let tap = UITapGestureRecognizer(target: self, action: action)
+        stack.addGestureRecognizer(tap)
+    }
+    
+    private func layoutStackLabel(label: UILabel, text: String) {
+        label.text = text
+        label.textColor = .gray
+        
+        label.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(15)
+            $0.centerY.equalToSuperview()
+        }
+    }
+    
+    private func layoutStackLeftArrow(stack: UIStackView) {
+        let arrowImageView = UIImageView()
+        
+        stack.addSubview(arrowImageView)
+        
+        arrowImageView.image = UIImage(systemName: "chevron.right")
+        arrowImageView.tintColor = .gray
+        
+        arrowImageView.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(15)
+            $0.centerY.equalToSuperview()
+        }
+    }
+    
+    private func layoutSeparatorUnderStack(separator: UIView, stack: UIStackView) {
+        separator.backgroundColor = .gray
+        
+        separator.snp.makeConstraints {
+            $0.top.equalTo(stack.snp.bottom)
+            $0.height.equalTo(1)
+            $0.leading.equalToSuperview().offset(15)
+            $0.trailing.equalToSuperview().inset(15)
+        }
+    }
+    
+    private func configureExitLabel() {
+        exitLabel.textColor = .red
+        exitLabel.isUserInteractionEnabled = true
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleExitLabelTap))
+        exitLabel.addGestureRecognizer(tap)
+    }
 
+    @objc func pushSwitchValueDidChange(sender: UISwitch!) {
+        if (sender.isOn){
+            print("Push switch is on")
+        }
+        else{
+            print("Push switch is off")
+        }
+    }
+    @objc func entrySwitchValueDidChange(sender: UISwitch!) {
+        if (sender.isOn){
+            print("Entry switch is on")
+        }
+        else{
+            print("Entry switch is off")
+        }
+    }
+    @objc func handlePasswordStackTap(sender: UITapGestureRecognizer? = nil) {
+        // handling code
+        print("Password stack clicked")
+        
+        let changePasswordVC = ChangePasswordViewController()
+        self.navigationController?.pushViewController(changePasswordVC, animated: true)
+    }
+    @objc func handleCodeStackTap(sender: UITapGestureRecognizer? = nil) {
+        // handling code
+        print("Code stack clicked")
+    }
+    @objc func handlePhoneStackTap(sender: UITapGestureRecognizer? = nil) {
+        // handling code
+        print("Phone stack clicked")
+    }
+    @objc func handleExitLabelTap(sender: UITapGestureRecognizer? = nil) {
+        // handling code
+        print("Exit label clicked")
+    }
+    @objc func handleImageViewTap(sender: UITapGestureRecognizer) {
+
+        // Your action
+        let picker = UIImagePickerController()
+        
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        var selectedImageFromPicker: UIImage?
+        
+        if let editedImage = info[.editedImage] as? UIImage {
+            selectedImageFromPicker = editedImage
+        }
+        else if let originalImage = info[.originalImage] as? UIImage {
+            selectedImageFromPicker = originalImage
+        }
+        
+        if let selectedImage = selectedImageFromPicker {
+            imageView.image = selectedImage
+        }
+         
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        print("cancel tapped")
+        dismiss(animated: true, completion: nil)
+    }
 }
 
